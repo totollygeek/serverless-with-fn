fe39cc4be8c768b49b7e22576675b865111c693a
Todor Todorov <totollygeek@users.noreply.github.com>
2019-05-29 14:34:04 +0300

    Added some comments to ps files + incremented versions

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -1,19 +1,40 @@
-fn create app devdays
+# This will do a clean deploy of an application in Fn and two functions,
+# along with the stats ASP.NET Core application
+# This script should be run from the .\scripts folder
 
+# We initialize some variables used below
+$appName = 'devdays'
+$mySqlContainerName = 'mysql0'
+
+# Delete the fn app if it is there
+fn delete app $appName
+
+# Create the app
+fn create app $appName
+
+# Change location to detect function
 Set-Location ..\src\detect
 
-fn --verbose deploy --app devdays --local
+# Deploy the function
+fn --verbose deploy --app $appName --local
 
+# Change location to the save function
 Set-Location ..\save
 
-fn --verbose deploy --app devdays --local
+# Deploy the function
+fn --verbose deploy --app $appName --local
 
-$DB_ADDRESS = docker inspect --type container -f '{{.NetworkSettings.IPAddress}}' mysql0
-fn config app devdays DB_ADDRESS $DB_ADDRESS
+$DB_ADDRESS = docker inspect --type container -f '{{.NetworkSettings.IPAddress}}' $mySqlContainerName
+fn config app $appName DB_ADDRESS $DB_ADDRESS
 
 $FN_ADDRESS = docker inspect --type container -f '{{.NetworkSettings.IPAddress}}' fnserver
-fn config app devdays FN_ADDRESS $FN_ADDRESS
+fn config app $appName FN_ADDRESS $FN_ADDRESS
 
+# Drop the stats container if there
+docker rm stats -f
+
+# Run the new one
 docker run --name=stats -e ASPNETCORE_ENVIRONMENT=Development -e DB_ADDRESS=$DB_ADDRESS -p 8888:80 -d stats:latest
 
+# Change location back to where we are
 Set-Location ..\..\scripts
